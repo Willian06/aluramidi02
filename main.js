43d1561be35487b1f55f6f2fc25302e1d21b5451
Willian06 <126635903+Willian06@users.noreply.github.com>
2023-10-27 16:13:19 -0300

    sdgs

--- main.js
+++ main.js
@@ -2,3 +2,43 @@ function TocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 document.querySelector('.tecla_pom').onclick=TocaSomPom
+
+function TocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclick=TocaSomPom
+
+function TocaSomTim() {
+    document.querySelector('#som_tecla_tim').play();
+}
+document.querySelector('.tecla_tim').onclick=TocaSomPom
+
+function TocaSomPuff() {
+    document.querySelector('#som_tecla_puff').play();
+}
+document.querySelector('.tecla_puff').onclick=TocaSomPom
+
+function TocaSomSplash() {
+    document.querySelector('#som_tecla_splash').play();
+}
+document.querySelector('.tecla_splash').onclick=TocaSomPom
+
+function TocaSomToim() {
+    document.querySelector('#som_tecla_toim').play();
+}
+document.querySelector('.tecla_toim').onclick=TocaSomPom
+
+function TocaSomPsh() {
+    document.querySelector('#som_tecla_psh').play();
+}
+document.querySelector('.tecla_psh').onclick=TocaSomPom
+
+function TocaSomTic() {
+    document.querySelector('#som_tecla_tic').play();
+}
+document.querySelector('.tecla_tic').onclick=TocaSomPom
+
+function TocaSomTom() {
+    document.querySelector('#som_tecla_tom').play();
+}
+document.querySelector('.tecla_tom').onclick=TocaSomPom
